2920c47235c6c072cc086dc438b8698a8aeaa6b9
张宇枫 <zhangyufeng.96@bytedance.com>
2020-07-13 00:26:29 +0800

    fix hdfs

--- train.sh
+++ train.sh
@@ -1,1 +1,7 @@
-python disen_arnold.py
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple tensorboardX
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple tqdm
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple torch
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple torchvision
+hdfs dfs -get hdfs://haruna/home/byte_arnold_hl_vc/yufeng/disen_rob/data
+hdfs dfs -get hdfs://haruna/home/byte_arnold_hl_vc/yufeng/disen_rob/rst_adv
+python3 disentangle_training.py --parallel --batch_size 3000
